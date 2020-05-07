f85f91a5dbf935abdf7700c5113fbd2455e396c5
Dmitriy Maluda <maluda223@gmail.com>
2020-05-07 10:30:58 +0300

    finished scroll

--- js/script.js
+++ js/script.js
@@ -271,109 +271,117 @@ const isMobile = md.mobile();
 
 const countSectionPosition = (sectionEq) => {
 
-  const position = sectionEq * -100;
-  if (isNaN(position))
-    console.error("передано не верное значение в countSectionPositon");
+    const position = sectionEq * -100;
+    if (isNaN(position))
+        console.error("передано не верное значение в countSectionPositon");
 
-  return position;
+    return position;
 };
 
 const resetActiveClass = (item, eq) => {
-  item.eq(eq)
-    .addClass("active")
-    .siblings()
-    .removeClass("active");
+    item
+        .eq(eq)
+        .addClass("active")
+        .siblings()
+        .removeClass("active");
 };
 
 const performTransition = (sectionEq) => {
-  if (inScroll) return;
+    if (inScroll) return;
 
-  inScroll = true;
+    inScroll = true;
 
-  const position = countSectionPosition(sectionEq);
-  const trasitionOver = 1000;
-  const mouseInertionOver = 300;
+    const position = countSectionPosition(sectionEq);
+    const trasitionOver = 1000;
+    const mouseInertionOver = 300;
 
-  resetActiveClass(sections, sectionEq);
+    resetActiveClass(sections, sectionEq);
 
-  display.css({
-    transform: `translateY(${position}%)`
-  });
+    display.css({
+        transform: `translateY(${position}%)`
+    });
 
-  setTimeout(() => {
-    resetActiveClass($(".scroll-menu__item"), sectionEq);
-    inScroll = false;
-  }, trasitionOver + mouseInertionOver);
+    setTimeout(() => {
+        resetActiveClass($(".scroll-menu__item"), sectionEq);
+        inScroll = false;
+
+        $('.scroll-menu__item')
+            .eq(sectionEq)
+            .addClass("scroll-menu__item--active")
+            .siblings()
+            .removeClass("scroll-menu__item--active");
+
+    }, trasitionOver + mouseInertionOver);
 };
 
 const scroller = () => {
-  const activeSection = sections.filter(".active");
-  const nextSection = activeSection.next();
-  const prevSection = activeSection.prev();
+    const activeSection = sections.filter(".active");
+    const nextSection = activeSection.next();
+    const prevSection = activeSection.prev();
 
-  return {
-    next() {
-      if (nextSection.length) {
-        performTransition(nextSection.index());
-      }
-    },
-    prev() {
-      if (prevSection.length) {
-        performTransition(prevSection.index());
-      }
-    },
-  };
+    return {
+        next() {
+            if (nextSection.length) {
+                performTransition(nextSection.index());
+            }
+        },
+        prev() {
+            if (prevSection.length) {
+                performTransition(prevSection.index());
+            }
+        }
+    };
 };
 
 $(window).on("wheel", (e) => {
-  const deltaY = e.originalEvent.deltaY;
-  const windowScroller = scroller();
+    const deltaY = e.originalEvent.deltaY;
+    const windowScroller = scroller();
 
-  if (deltaY > 0) {
-    windowScroller.next();
-  }
+    if (deltaY > 0) {
+        windowScroller.next();
+    }
 
-  if (deltaY < 0) {
-    windowScroller.prev();
-  }
+    if (deltaY < 0) {
+        windowScroller.prev();
+    }
 });
 
 $(document).on("keydown", (e) => {
-  const tagName = e.target.tagName.toLowerCase();
-  const windowScroller = scroller();
-  const userTypingInInputs = tagName === "input" || tagName === "textarea";
+    const tagName = e.target.tagName.toLowerCase();
+    const windowScroller = scroller();
+    const userTypingInInputs = tagName === "input" || tagName === "textarea";
 
-  if (userTypingInInputs) return;
+    if (userTypingInInputs) return;
 
-  switch (e.keyCode) {
-    case 38:
-      windowScroller.prev();
-      break;
-    case 40:
-      windowScroller.next();
-      break;
-  }
+    switch (e.keyCode) {
+        case 38:
+            windowScroller.prev();
+            break;
+        case 40:
+            windowScroller.next();
+            break;
+    }
 });
 
-$("[data-scroll-to]").on("click", (e) => {
+$('[data-scroll-to]').on("click", e => {
 
-  const $this = $(e.currentTarget);
-  const target = $this.attr("data-scroll-to");
+    const $this = $(e.currentTarget);
+    const target = $this.attr("data-scroll-to");
 
-  performTransition(target);
+    performTransition(target);
 });
 
 if (isMobile) {
-  // https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
-  $("body").swipe({
-    swipe: (event, direction) => {
-      let scrollDirection;
-      const windowScroller = scroller();
+    // https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
+    $("body").swipe({
+        swipe: (event, direction) => {
+            let scrollDirection;
+            const windowScroller = scroller();
 
-      if (direction === "up") scrollDirection = "next";
-      if (direction === "down") scrollDirection = "prev";
+            if (direction === "up") scrollDirection = "next";
+            if (direction === "down") scrollDirection = "prev";
 
-      windowScroller[scrollDirection]();
-    },
-  });
+            windowScroller[scrollDirection]();
+        },
+    });
 }
